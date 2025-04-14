b9a2d18dd446b76fdee76f281c48c022bdb03b3f
rekloyd <paumorillasrivera129@gmail.com>
2025-04-14 13:10:04 +0200

    blog now uses xport function and has similar nav

--- public/blog/postManager.js
+++ public/blog/postManager.js
@@ -1,26 +1,20 @@
-import { Blog } from './Blog.js';
 import { Post } from './Post.js';
 
-const blog = new Blog();
+export function renderPosts(blog) {
+  const posts = blog.obtenerPosts();
+  const postContainer = document.getElementById('post-container');
+  postContainer.innerHTML = ''; // Limpiamos el contenedor de posts
 
-// Simulación de carga de posts
-blog.agregarPost(new Post("Primer Post", "Este es el contenido del primer post", "2024-04-14"));
-blog.agregarPost(new Post("Segundo Post", "Contenido del segundo post interesante", "2024-04-13"));
-
-export function renderPosts(containerId = 'post-container') {
-  const container = document.getElementById(containerId);
-  container.innerHTML = '';
-
-  blog.obtenerPosts().forEach(post => {
+  posts.forEach(post => {
     const postElement = document.createElement('div');
     postElement.classList.add('post');
-
+    
     postElement.innerHTML = `
       <h2>${post.titulo}</h2>
       <p>${post.contenido}</p>
       <small>${post.fecha}</small>
     `;
 
-    container.appendChild(postElement);
+    postContainer.appendChild(postElement);
   });
 }
